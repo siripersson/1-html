a757cca14fce047212e3f72aa186353f33fc3477
siripersson <siri.persson1@gmail.com>
2017-09-26 11:19:25 +0200

    Testing things from lecture

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 
-const mainheader= document.getElementById('main-header')
-mainheader.textContent= "Javascript is acting!"
+const mainhead= document.getElementById("main-header")
+mainhead.textContent= "Javascript is acting!"
+
+// En funktion för att skicka mail till en användare
+const sendEmail =  (email) => {
+	console.log("Sending email to user")
+}
+
+// Lista med emails
+const emails= [{email:"anna@gmail.com", name: "Anna"} ,{email: "per@gmail.com", name: "Per"}]
+for(i=0; i<emails.length; i++){
+	console.log("Emails: ", emails[i].email)
+}
